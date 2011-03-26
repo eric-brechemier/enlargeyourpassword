64fefe7624cf5193b1933c781cbe42e9977aa83b
Eric Bréchemier <github@eric.brechemier.name>
2011-03-26 19:42:52 +0100

    Added base structure for main JavaScript script

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*
+ * File:      main.js - Main Script for enlargeyourpassword.com
+ *
+ * Author:    Eric Bréchemier <github@eric.brechemier.name>
+ * License:   Creative Commons Attribution 3.0 Unported
+ *            http://creativecommons.org/licenses/by/3.0/
+ * Version:   2011-03-26
+ */
+/*jslint nomen:false, white:false, onevar:false, plusplus:false */
+/*global document */
+(function() {
+  // Closure to prive private scope
+
+  
+
+}());
